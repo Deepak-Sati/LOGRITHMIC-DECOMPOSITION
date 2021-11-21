29805df64ee91403e1a8ae1e5fa17d32b60218bd
Github <52237525+Deepak-Sati@users.noreply.github.com>
2021-11-21 14:13:34 +0530

    Update Factor_Tree.cpp

--- Factor_Tree.cpp
+++ Factor_Tree.cpp
@@ -9,7 +9,7 @@ using namespace std;
 const int maxN = 1e5+5, maxA = 1e6+5, DPN = 20;
 int blk, timer, n, q;
 
-vector<vector<int> > g;
+vector< vector<int> > g;
 ll  ans_query[maxN], inv[20*maxN], ans;
 
 int spf[maxA], a[maxN], cnt[maxA], vis[maxN], lvl[maxN];
@@ -21,12 +21,13 @@ struct Query
 {
 	int l, r;
 	int lca, ind;
-	bool operator <(const Query &q1){
+	bool operator < (const Query &q1)
+	{
 	    if(l/blk != q1.l/blk)
 	       return l < q1.l;
 	    else
 	       return l/blk % 2 ? r < q1.r : r > q1.r;
-    }
+        }
 }qr[maxN];
 
 // DFS for in-out time of node and dp for binary Lifting
@@ -37,10 +38,10 @@ void dfs(int c,int p)
     dp[c][0]    = p;
     lvl[c]      = lvl[p] + 1;
 
-    for(int i=1;i<DPN;i++)
+    for( int i = 1; i < DPN; i++ )
        dp[c][i] = dp[dp[c][i-1]][i-1];
     
-    for(auto k:g[c])
+    for( auto k:g[c] )
         if(k != p)
           dfs(k,c);
     tout[c] = ++timer;
@@ -59,7 +60,8 @@ int lca(int u, int v)
         return u;
     if (is_ancestor(v, u))
         return v;
-    for (int i = DPN-1; i >= 0; --i) {
+    for ( int i = DPN-1; i >= 0; --i ) 
+    {
         if (!is_ancestor(dp[u][i], v))
             u = dp[u][i];
     }
@@ -69,23 +71,26 @@ int lca(int u, int v)
 // prime seive of eratosthenes
 void precompute()
 {
-    for(int i=1;i<maxA;i++)
-        spf[i]=i;
-    for(int i=2;i*i<maxA;i++)
-        if(spf[i]==i)
-           for(int j=i*i;j<maxA;j+=i)
-               if(spf[j]==j)
-                  spf[j]=i;
-    inv[0]=inv[1]=1;
-  	for(int i=2;i<20*maxN;i++)
+    for( int i = 1; i < maxA; i++ )
+        spf[i] = i;
+	
+    for( int i = 2; i*i < maxA; i++ )
+        if( spf[i] == i )
+           for( int j = i*i ; j < maxA; j+=i )
+               if( spf[j] == j )
+                  spf[j] = i;
+    
+	inv[0] = inv[1] = 1;
+  	for( int i = 2; i < 20*maxN; i++ )
 	      inv[i] = (MOD - (((MOD/i) * 1ll *inv[MOD % i])  % MOD)) % MOD;
 }
 
 // For adding/removing factors of number
-void factorise(int ind,int add)
+void factorise(int ind, int add)
 {
     int x = a[ind], p = -1;
-    while(x!=1){
+    while( x!=1 )
+    {
           if(p != spf[x]){
              ans = ( ans * inv[cnt[spf[x]]+1] ) % MOD;
              if(p != -1)
@@ -105,14 +110,15 @@ void factorise(int ind,int add)
 void update(int x)
 {
     vis[x] = !vis[x];
-    factorise(x,vis[x]);
+    factorise(x, vis[x]);
 }
 
 // Standard Mo's Algorithm
 void MO(int q)
 {
-    int ml=0,mr=-1;
-  	for(int i=0;i<q;i++) {
+        int ml=0,mr=-1;
+  	for( int i = 0 ; i < q ; i++ ) 
+	{
 		int L   = qr[i].l;
 		int R   = qr[i].r;
 
@@ -137,8 +143,9 @@ void solve()
     cin>>n;
     g = vector<vector<int> >(n);
     timer=0;
-    for(int i=0;i<n-1;i++){
-        int u,v;cin>>u>>v;
+    for( int i = 0 ; i < n-1 ; i++)
+    {
+        int u,v; cin >> u >> v;
         g[u-1].push_back(v-1);
         g[v-1].push_back(u-1);
     }
@@ -146,17 +153,21 @@ void solve()
         cin >> a[i];
     
     dfs(0,0);
+    cin >> q;
     
-    cin>>q;
-    for(int i=0;i<q;i++){
+    for( int i = 0 ; i < q; i++ )
+    {
         int u,v;cin>>u>>v;
         --u;--v;
-        qr[i].ind = i;
+        
+	qr[i].ind = i;
         qr[i].lca = lca(u,v);
-        //debug1(qr[i].lca);
-        if(tin[u]>tin[v])
+        
+	//debug1(qr[i].lca);
+        if(tin[u] > tin[v])
             swap(u,v);
-        if(qr[i].lca==u){
+        if(qr[i].lca == u)
+	{
             qr[i].l = tin[u];
             qr[i].r = tin[v];
         }
@@ -172,7 +183,7 @@ void solve()
     memset(vis,0,sizeof(vis));
     ans=1;
     MO(q);
-    for(int i=0;i<q;i++)
+    for( int i = 0; i < q; i++ )
         cout << ans_query[i] << endl;
 }
 
@@ -181,6 +192,7 @@ int main()
 flash;
 int t;cin>>t;
 // Seive for smallest prime factor
+	
 precompute();                                
 while(t--)
     solve();
